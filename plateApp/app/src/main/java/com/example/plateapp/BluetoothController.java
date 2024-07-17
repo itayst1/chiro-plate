fd276e4d0f8242e67646eddc3417b9af2714843f
Itay Steinberg <itay.steinberg2007@gmail.com>
2024-07-17 20:51:19 +0300

    moved all bluetooth related stuff to a single class
    bluetooth class has single instance so it can be used throughout the app freely

---
+++ plateApp/app/src/main/java/com/example/plateapp/BluetoothController.java
@@ -0,0 +1,186 @@
+package com.example.plateapp;
+
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class BluetoothController {
+
+    private static BluetoothController m_instance = null;
+
+    private BluetoothAdapter m_bluetoothAdapter;
+
+    private ArrayList<BluetoothDevice> m_listItems = new ArrayList<>();
+
+    public static BluetoothController getInstance(){
+        if(m_instance == null)
+            m_instance = new BluetoothController();
+        return m_instance;
+    }
+
+    private BluetoothController(){}
+
+    public void startBluetoothScan() {
+        try {
+            m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (!m_bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                return;
+            }
+            scanLeDevice();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean scanning = false;
+    private Handler handler = new Handler();
+
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 5000;
+
+    @SuppressLint("MissingPermission")
+    private void scanLeDevice() {
+        new Thread(() -> {
+            if (!scanning) {
+                // Stops scanning after a predefined scan period.
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        scanning = false;
+                        m_bluetoothAdapter.getBluetoothLeScanner().stopScan(leScanCallback);
+                    }
+                }, SCAN_PERIOD);
+
+                scanning = true;
+                m_listItems.clear();
+                m_bluetoothAdapter.getBluetoothLeScanner().startScan(leScanCallback);
+            } else {
+                scanning = false;
+                m_bluetoothAdapter.getBluetoothLeScanner().stopScan(leScanCallback);
+            }
+        }).start();
+    }
+
+    @SuppressLint("MissingPermission")
+    private final ScanCallback leScanCallback = new ScanCallback() {
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            String name = result.getDevice().getName();
+            if (name != null && !m_listItems.contains(result.getDevice())) {
+                m_listItems.add(result.getDevice());
+            }
+        }
+    };
+
+    public ArrayList<BluetoothDevice> getItemList() {
+        return m_listItems;
+    }
+
+
+    //gatt
+
+    private final BroadcastReceiver disconnectBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(m_bluetoothGatt != null)
+                disconnect(); // disconnect now, else would be queued until UI re-attached
+        }
+    };;
+
+    private BluetoothGatt m_bluetoothGatt = null;
+
+    private String m_connectedDevice = "";
+
+    private BluetoothGattCharacteristic m_writeCharacteristic, m_readCharacteristic;
+
+    public BluetoothGatt getBluetoothGatt() {return m_bluetoothGatt;}
+
+    public String getConnectedDevice() {return m_connectedDevice;}
+    public void setConnectedDevice(String device) {m_connectedDevice = device;}
+
+    @SuppressLint("MissingPermission")
+    public void connectSelected(String name, Context context){
+        for(BluetoothDevice device : BluetoothController.getInstance().getItemList()){
+            if(device.getName().equals(name)){
+                disconnect();
+                ContextCompat.registerReceiver(context, disconnectBroadcastReceiver, new IntentFilter("Disconnect"), ContextCompat.RECEIVER_NOT_EXPORTED);
+                m_bluetoothGatt = device.connectGatt(context, false, bluetoothGattCallback);
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    public void disconnect(){
+        if(m_bluetoothGatt != null){
+            m_bluetoothGatt.disconnect();
+            m_bluetoothGatt.close();
+            m_bluetoothGatt = null;
+            m_connectedDevice = "";
+        }
+    }
+
+    private final BluetoothGattCallback bluetoothGattCallback = new BluetoothGattCallback() {
+        @SuppressLint("MissingPermission")
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                m_bluetoothGatt.discoverServices();
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                // disconnected from the GATT Server
+            }
+        }
+
+        @Override
+        public void onCharacteristicChanged(@NonNull BluetoothGatt gatt, @NonNull BluetoothGattCharacteristic characteristic, @NonNull byte[] value) {
+            super.onCharacteristicChanged(gatt, characteristic, value);
+            Log.d("read", characteristic.getStringValue(0));
+        }
+
+        @SuppressLint("MissingPermission")
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                m_writeCharacteristic = m_bluetoothGatt.getServices().get(2).getCharacteristics().get(1);
+                m_writeCharacteristic.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE);
+
+                m_readCharacteristic = m_bluetoothGatt.getServices().get(2).getCharacteristics().get(0);
+                m_readCharacteristic.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE);
+                m_bluetoothGatt.setCharacteristicNotification(m_readCharacteristic, true);
+                BluetoothGattDescriptor desc = m_readCharacteristic.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"));
+                desc.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                gatt.writeDescriptor(desc);
+                Log.i("characteristics", "done");
+            } else {
+                Log.w("TAG", "onServicesDiscovered received: " + status);
+            }
+        }
+    };
+
+    @SuppressLint("MissingPermission")
+    public void writeCharacteristic(String data) {
+        m_writeCharacteristic.setValue(data.getBytes());
+        m_bluetoothGatt.writeCharacteristic(m_writeCharacteristic);
+    }
+
+
+}
--- plateApp/app/src/main/java/com/example/plateapp/MainActivity.java
+++ plateApp/app/src/main/java/com/example/plateapp/MainActivity.java
@@ -1,55 +1,27 @@
 package com.example.plateapp;
 
-import static android.nfc.NfcAdapter.EXTRA_DATA;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattDescriptor;
-import android.bluetooth.BluetoothGattService;
-import android.bluetooth.BluetoothProfile;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button scan;
     private Button toggle;
     private TableLayout items;
-    private BluetoothUtils bluetoothUtils = new BluetoothUtils();
-
-    BroadcastReceiver disconnectBroadcastReceiver;
-
-    private BluetoothGatt bluetoothGatt = null;
-
-    private String connectedDevice = "";
-
-    private BluetoothGattCharacteristic writeCharacteristic, readCharacteristic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,92 +38,12 @@ public class MainActivity extends AppCompatActivity {
         items.setPadding(0, 30, 0, 0);
         scan = (Button) findViewById(R.id.scan);
         toggle = (Button) findViewById(R.id.toggle);
-
-        disconnectBroadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if(bluetoothGatt != null)
-                    disconnect(); // disconnect now, else would be queued until UI re-attached
-            }
-        };
     }
-
-    @SuppressLint("MissingPermission")
-    private void connectSelected(String name){
-        for(BluetoothDevice device : bluetoothUtils.getItemList()){
-            if(device.getName().equals(name)){
-                disconnect();
-                ContextCompat.registerReceiver(this, disconnectBroadcastReceiver, new IntentFilter("Disconnect"), ContextCompat.RECEIVER_NOT_EXPORTED);
-                bluetoothGatt = device.connectGatt(this, false, bluetoothGattCallback);
-            }
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private void disconnect(){
-        if(bluetoothGatt != null){
-            bluetoothGatt.disconnect();
-            bluetoothGatt.close();
-            bluetoothGatt = null;
-            this.unregisterReceiver(disconnectBroadcastReceiver);
-            connectedDevice = "";
-        }
-    }
-
-    private final BluetoothGattCallback bluetoothGattCallback = new BluetoothGattCallback() {
-        @SuppressLint("MissingPermission")
-        @Override
-        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-            if (newState == BluetoothProfile.STATE_CONNECTED) {
-                bluetoothGatt.discoverServices();
-            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                // disconnected from the GATT Server
-            }
-        }
-
-        @Override
-        public void onCharacteristicChanged(@NonNull BluetoothGatt gatt, @NonNull BluetoothGattCharacteristic characteristic, @NonNull byte[] value) {
-            super.onCharacteristicChanged(gatt, characteristic, value);
-            Log.d("read", characteristic.getStringValue(0));
-        }
-
-        @SuppressLint("MissingPermission")
-        @Override
-        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-            if (status == BluetoothGatt.GATT_SUCCESS) {
-                writeCharacteristic = bluetoothGatt.getServices().get(2).getCharacteristics().get(1);
-                writeCharacteristic.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE);
-
-                readCharacteristic = bluetoothGatt.getServices().get(2).getCharacteristics().get(0);
-                readCharacteristic.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE);
-                bluetoothGatt.setCharacteristicNotification(readCharacteristic, true);
-                BluetoothGattDescriptor desc = readCharacteristic.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"));
-                desc.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
-                gatt.writeDescriptor(desc);
-                Log.i("characteristics", "done");
-            } else {
-                Log.w("TAG", "onServicesDiscovered received: " + status);
-            }
-        }
-    };
-
-    @SuppressLint("MissingPermission")
-    public void writeCharacteristic(String data) {
-        writeCharacteristic.setValue(data.getBytes());
-        bluetoothGatt.writeCharacteristic(writeCharacteristic);
-    }
-
-    @SuppressLint("MissingPermission")
-    public void readCharacteristic() {
-        bluetoothGatt.readCharacteristic(readCharacteristic);
-    }
-
-
 
     @SuppressLint("MissingPermission")
     public void onToggleClick(View view) {
-        if(bluetoothGatt != null){
-            writeCharacteristic("toggle\n\r");
+        if(BluetoothController.getInstance().getBluetoothGatt() != null){
+            BluetoothController.getInstance().writeCharacteristic("toggle\n\r");
         }
     }
 
@@ -162,8 +54,8 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        disconnect();
-        bluetoothUtils.startBluetoothScan();
+        BluetoothController.getInstance().disconnect();
+        BluetoothController.getInstance().startBluetoothScan();
         scan.setText(R.string.scanning);
         items.removeAllViews();
         scan.setEnabled(false);
@@ -172,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 scan.setText(R.string.start_scan);
                 Button temp;
-                for (BluetoothDevice device : bluetoothUtils.getItemList()) {
+                for (BluetoothDevice device : BluetoothController.getInstance().getItemList()) {
                     @SuppressLint("MissingPermission") String name = device.getName();
                     if (name != null) {
                         temp = new Button(MainActivity.this);
@@ -186,13 +78,13 @@ public class MainActivity extends AppCompatActivity {
                                 for(int i = 0; i < items.getChildCount(); i++){
                                     ((Button)items.getChildAt(i)).setTextColor(0xFFFFFFFF);
                                 }
-                                if(!connectedDevice.equals(((Button) view).getText().toString())) {
+                                if(!BluetoothController.getInstance().getConnectedDevice().equals(((Button) view).getText().toString())) {
                                     ((Button) view).setTextColor(0xFF00FF00);
-                                    connectSelected(((Button) view).getText().toString());
-                                    connectedDevice = ((Button) view).getText().toString();
+                                    BluetoothController.getInstance().connectSelected(((Button) view).getText().toString(), MainActivity.this);
+                                    BluetoothController.getInstance().setConnectedDevice(((Button) view).getText().toString());
                                 }
-                                else if(connectedDevice.equals(((Button) view).getText().toString())){
-                                    disconnect();
+                                else if(BluetoothController.getInstance().getConnectedDevice().equals(((Button) view).getText().toString())){
+                                    BluetoothController.getInstance().disconnect();
                                 }
                             }
                         });
