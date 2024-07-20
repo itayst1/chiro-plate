42c0887c0660e62c1f36af3b93a1eebb8498aaa1
Itay Steinberg <itay.steinberg2007@gmail.com>
2024-07-20 20:41:35 +0300

    added pin reading for raspberry program

--- BluetoothServer.py
+++ BluetoothServer.py
@@ -9,22 +9,41 @@ ble = bluetooth.BLE()
 # Create an instance of the BLESimplePeripheral class with the BLE object
 sp = BLESimplePeripheral(ble)
 
-# Create a Pin object for the onboard LED, configure it as an output
-led = Pin("LED", Pin.OUT)
+# Create a Pin object for the sensors, configure it as an input
+pin1 = Pin(18, Pin.IN)
+pin2 = Pin(19, Pin.IN)
+pin3 = Pin(20, Pin.IN)
+pin4 = Pin(21, Pin.IN)
 
-# Initialize the LED state to 0 (off)
-led_state = 0
+scored = False
+activatedSensorIdx = 0
+
+def run():
+    global scored
+    global activatedSensorIdx
+    if scored == False:
+        if pin1.value() == 1:
+            activatedSensorIdx = 1
+        elif pin2.value() == 1:
+            activatedSensorIdx = 2
+        elif pin3.value() == 1:
+            activatedSensorIdx = 3
+        elif pin4.value() == 1:
+            activatedSensorIdx = 4
+    if activatedSensorIdx > 0:
+        scored = True
+    else:
+        scored = False
+    return activatedSensorIdx
 
 # Define a callback function to handle received data
 def on_rx(data):
     print("Data received: ", data)  # Print the received data
-    global led_state  # Access the global variable led_state
-    if data == b'toggle\r\n':  # Check if the received data is "toggle"
-        led.value(not led_state)  # Toggle the LED state (on/off)
-        led_state = 1 - led_state  # Update the LED state
+    if data == b'reset':  # Check if the received data is "reset"
+        # write here what to do
 
 # Start an infinite loop
 while True:
     if sp.is_connected():  # Check if a BLE connection is established
-        sp.on_write(on_rx)  # Set the callback function for data reception
-        sp.send("5")
+#         sp.on_write(on_rx)  # Set the callback function for data reception
+        sp.send(run())  #sends data
