b034af5804b7d87baa82a2590d33da8cf844f59e
Itay Steinberg <itay.steinberg2007@gmail.com>
2024-08-11 19:22:40 +0300

    fixed pin reading

--- BluetoothServer.py
+++ BluetoothServer.py
@@ -1,4 +1,5 @@
 # Import necessary modules
+import time
 from machine import Pin 
 import bluetooth
 from ble_simple_peripheral import BLESimplePeripheral
@@ -19,31 +20,29 @@ scored = False
 activatedSensorIdx = 0
 
 def run():
-    global scored
     global activatedSensorIdx
-    if scored == False:
-        if pin1.value() == 1:
-            activatedSensorIdx = 1
-        elif pin2.value() == 1:
-            activatedSensorIdx = 2
-        elif pin3.value() == 1:
-            activatedSensorIdx = 3
-        elif pin4.value() == 1:
-            activatedSensorIdx = 4
-    if activatedSensorIdx > 0:
-        scored = True
+    if pin1.value() == 1:
+        activatedSensorIdx = 1
+    elif pin2.value() == 1:
+        activatedSensorIdx = 2
+    elif pin3.value() == 1:
+        activatedSensorIdx = 3
+    elif pin4.value() == 1:
+        activatedSensorIdx = 4
     else:
-        scored = False
+        activatedSensorIdx = 0
     return activatedSensorIdx
 
 # Define a callback function to handle received data
 def on_rx(data):
     print("Data received: ", data)  # Print the received data
-    if data == b'reset':  # Check if the received data is "reset"
+    #if data == b'reset':  # Check if the received data is "reset"
         # write here what to do
 
 # Start an infinite loop
 while True:
     if sp.is_connected():  # Check if a BLE connection is established
 #         sp.on_write(on_rx)  # Set the callback function for data reception
-        sp.send(run())  #sends data
+#         sp.send(run())  #sends data
+        print(run())
+        time.sleep(0.01)
